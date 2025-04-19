1b7f10aac24543d8d1b88b8e3845535c71b71096
Gilberto Garza Castillo <146159402+GilbertC17@users.noreply.github.com>
2025-04-18 18:18:26 -0600

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createConnection({
+  host: 'srv654.hstgr.io',
+  user: 'u108791874_admin', // Cambia esto si tu usuario de MySQL es diferente
+  password: 'Dragonb100', // Cambia esto si tienes una contraseña configurada
+  database: 'u108791874_login_demo',
+});
+
+// Conexión a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos');
+});
+
+module.exports = db;
